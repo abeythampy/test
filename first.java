dc6acbea599421f0097003853c3b8594b9ed0ce9
athampy@fintech.net <abeythampy@ZPCOKMAC00111.local>
2023-02-23 10:19:33 +0530

    first

---
+++ first.java
@@ -0,0 +1,8 @@
+import com.abey;
+class first{
+public static void main(String a[]){
+  system.out.println("Git Test1");
+  system.out.println("Git Test2");
+  system.out.println("Git Test3");
+}
+}
